ef80d62d8dd0035d5e8e55057f291d4fd0b5621f
Caprena Richie <robcaprena@gmail.com>
2021-10-20 10:45:52 -0400

    first commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PalindromeExercise;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word.ToLower()[i];
+            }
+            if(reversed == word.ToLower())
+            {
+                throw new NotImplementedException();
+                //return true;
+            }
+            else
+            {
+                throw new NotImplementedException();
+                //return false;
+            }
+            
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("civic", true)]
+        [InlineData("blooper", false)]
+        [InlineData("Racecar", true)]
+        public void IsAPalindrome(string answer, bool expected)
         {
+            //Arrange
+             var wrdSmth = new WordSmith();
+
+            //Act
+            var actual = wrdSmth.IsAPalindrome(answer);
+
+            Assert.Equal(expected, actual);
+
+
 
         }
     }
